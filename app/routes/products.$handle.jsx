e9267a7b1e109fd76b36c1e225a9cbeb566dcae1
Diane Russel <dianerussel@rogers.com>
2023-06-16 19:14:03 -0400

    add shop pay button

--- app/routes/products.$handle.jsx
+++ app/routes/products.$handle.jsx
@@ -1,6 +1,6 @@
 import {useLoaderData} from '@remix-run/react';
 import {json} from '@shopify/remix-oxygen';
-import {MediaFile} from '@shopify/hydrogen-react';
+import {MediaFile, Money, ShopPayButton} from '@shopify/hydrogen-react';
 import ProductOptions from '~/components/ProductOptions';
 
 const seo = ({data}) => ({
@@ -16,6 +16,8 @@ export async function loader({params, context, request}) {
   const {handle} = params;
   const searchParams = new URL(request.url).searchParams;
   const selectedOptions = [];
+
+  const storeDomain = context.storefront.getShopifyDomain();
 
   // set selected options from the query string
   searchParams.forEach((value, name) => {
@@ -41,10 +43,12 @@ export async function loader({params, context, request}) {
   return json({
     product,
     selectedVariant,
+    storeDomain,
   });
 }
 export default function ProductHandle() {
-  const {product, selectedVariant} = useLoaderData();
+  const {product, selectedVariant, storeDomain} = useLoaderData();
+  const orderable = selectedVariant?.availableForSale || false;
 
   return (
     <section className="w-full gap-4 md:gap-8 grid px-6 md:px-8 lg:px-12">
@@ -72,6 +76,22 @@ export default function ProductHandle() {
             options={product.options}
             selectedVariant={selectedVariant}
           />
+          <Money
+            withoutTrailingZeros
+            data={selectedVariant.price}
+            className="text-xl font-semibold mb-2"
+          />
+          {orderable && (
+            <div className="space-y-2">
+              <ShopPayButton
+                storeDomain={storeDomain}
+                variantIds={[selectedVariant?.id]}
+                width={'400px'}
+              />
+              {/* TODO product form */}
+            </div>
+          )}
+
           <div
             className="prose border-t border-gray-200 pt-6 text-black text-md"
             dangerouslySetInnerHTML={{__html: product.descriptionHtml}}
